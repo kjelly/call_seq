fdd8b640bcb9cb7fa4a4458a0a4dc7c426151a55
ya790206 <ya790206@yahoo.com.tw>
2014-06-16 21:34:48 +0800

    update ui because json format is changed.

--- call_seq/browser.py
+++ call_seq/browser.py
@@ -15,6 +15,16 @@ class MainWindow(QtGui.QMainWindow):
         self.setCentralWidget(self.main_editor)
         self.createDockWindows()
         self.createMenuBar()
+        self.createContextMenu()
+
+    def createContextMenu(self):
+        self.context_menu = QtGui.QMenu(self.tr("Caller"), self)
+        self.caller_action = self.context_menu.addAction(self.tr("Calle&r"))
+        self.caller_action.triggered.connect(self.show_caller_code)
+        self.callee_action = self.context_menu.addAction(self.tr("Calle&e"))
+        self.callee_action.triggered.connect(self.show_callee_code)
+
+
 
     def createMenuBar(self):
         openAction = QtGui.QAction('&open', self)
@@ -31,7 +41,9 @@ class MainWindow(QtGui.QMainWindow):
     def createDockWindows(self):
         self.tree_docker = QtGui.QDockWidget("Call Sequence", self)
         self.tree_widget = QtGui.QTreeWidget(self.tree_docker)
-        self.tree_widget.itemClicked.connect(self.item_clicked)
+        self.tree_widget.itemClicked.connect(self.show_caller_code)
+        self.tree_widget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.tree_widget.customContextMenuRequested.connect(self.on_custom_context_menu)
         self.tree_docker.setWidget(self.tree_widget)
         self.addDockWidget(QtCore.Qt.LeftDockWidgetArea,
                            self.tree_docker)
@@ -63,24 +75,47 @@ class MainWindow(QtGui.QMainWindow):
             if i.get('name', None):
                 item.setText(0, i['name'])
             else:
-                item.setText(0, i['code'])
+                item.setText(0, i['caller_code'])
             item.meta_data = i
             name = self.build_tree(item, i)
             top.addChild(item)
 
-    def item_clicked(self, item, column):
+    def show_caller_code(self, *args):
+        item = self.tree_widget.currentItem()
         meta_data = item.meta_data
-        file_name = meta_data['file_name']
-        if not os.path.exists(file_name):
-            return
-        self.main_editor.openFile(file_name)
-        self.main_editor.gotoLine(meta_data['lineno'])
 
-        info_data = copy.copy(meta_data)
+        self.go_to_file_line(meta_data['caller_file_name'], meta_data['lineno'])
+        self.show_infomation(item)
+        return True
+
+    def show_callee_code(self, *args):
+        item = self.tree_widget.currentItem()
+        meta_data = item.meta_data
+
+        self.go_to_file_line(meta_data['callee_file_name'], meta_data['callee_first_line'])
+        self.show_infomation(item)
+        return True
+
+    def show_infomation(self, item):
+
+        info_data = copy.copy(item.meta_data)
         del info_data['seq']
         self.info_widget.setText(json.dumps(info_data,
                                             sort_keys=True,indent=4,
                                             separators=(',', ': ')))
+
+    def go_to_file_line(self, file_name, lineno):
+        if not os.path.exists(file_name):
+            return
+        self.main_editor.openFile(file_name)
+        self.main_editor.gotoLine(lineno)
+
+
+    def on_custom_context_menu(self, point):
+        index = self.tree_widget.indexAt(point)
+        if index.isValid():
+            self.context_menu.exec_(self.tree_widget.mapToGlobal(point))
+
 
 
 def main():
