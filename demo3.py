ab9291450084666d1c6a85676a7e121fdf6b3d20
ya790206 <ya790206@yahoo.com.tw>
2014-06-19 23:19:27 +0800

    a example for how to use call_seq using decorator.

---
+++ demo3.py
@@ -0,0 +1,25 @@
+from call_seq import trace
+
+
+def test2():
+    pass
+
+
+def test1():
+    test2()
+    return 1 + 1
+
+
+def test():
+    test1()
+    print 'call me'
+    test1()
+
+
+@trace('output.json')
+def main():
+    test()
+
+
+if __name__ == '__main__':
+    main()
