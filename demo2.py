0953a4274fd98c6ded668ef80532e2a588cb0f6f
ya790206 <ya790206@yahoo.com.tw>
2014-06-19 23:05:22 +0800

    add demo file to show using `with` block to record how the program run.

---
+++ demo2.py
@@ -0,0 +1,21 @@
+from call_seq import CallSeq
+
+
+def test2():
+    pass
+
+
+def test1():
+    test2()
+    return 1 + 1
+
+
+def test():
+    test1()
+    print 'call me'
+    test1()
+
+
+if __name__ == '__main__':
+    with CallSeq(name='output.json'):
+        test()
