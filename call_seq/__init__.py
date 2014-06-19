bc83296a47a5e1eeba053f965e4847d35589532c
ya790206 <ya790206@yahoo.com.tw>
2014-06-19 23:16:19 +0800

    add decorator function for tracing code more eaisly.

--- call_seq/__init__.py
+++ call_seq/__init__.py
@@ -1,1 +1,14 @@
 from .core import CallSeq
+
+
+def trace(name):
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            trail = CallSeq()
+            trail.set_trace()
+            ret = func(*args, **kwargs)
+            trail.unset_trace()
+            trail.dump_to_file(name)
+            return ret
+        return inner
+    return wrapper
