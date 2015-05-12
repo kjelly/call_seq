737884884e69ec7f29c64ec4a45dce9c102370d7
ya790206 <ya790206@gmail.com>
2015-05-12 22:26:53 +0800

    add max_depth argument for trace function.

--- call_seq/__init__.py
+++ call_seq/__init__.py
@@ -1,10 +1,10 @@
 from .core import CallSeq
 
 
-def trace(name):
+def trace(name, max_depth=None):
     def wrapper(func):
         def inner(*args, **kwargs):
-            trail = CallSeq()
+            trail = CallSeq(max_depth=max_depth)
             trail.set_trace()
             ret = func(*args, **kwargs)
             trail.unset_trace()
